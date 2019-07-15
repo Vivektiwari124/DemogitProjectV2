57e4abe1148faeadbe8d90230070a81c7aab5b0c
admin <admin@192.168.97.131>
2019-07-15 20:44:14 +0530

    first time commit code

---
+++ src/demoGitPackage/TestClass1.java
@@ -0,0 +1,15 @@
+package demoGitPackage;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "Yes")
+			System.out.println("eat Something");
+		else {
+			System.out.println("Do Work...");
+		}
+
+	}
+
+}
