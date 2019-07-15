e5ea3dc413a874a28f54a45cb399d4389b6fb3a0
vivek.tiwari <vivek.tiwari@Vivek-Tiwari1.diasparkindia.com>
2019-07-15 21:45:55 +0530

    changes has been done in logic

--- src/demoGitPackage/TestClass2.java
+++ src/demoGitPackage/TestClass2.java
@@ -4,7 +4,7 @@ public class TestClass2 {
 
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if (hungry == "Yes")
+		if (hungry == "Yes" || hungry == "z")
 			System.out.println("eat Something");
 		else {
 			System.out.println("Do Work...");
