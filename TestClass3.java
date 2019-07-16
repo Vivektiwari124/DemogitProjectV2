e890e9fde43e4f7a5888179635b62e7fd7e7b680
Vivek Tiwari <vivek.tiwari@diaspark.com>
2019-07-16 18:06:59 +0530

    add new files

---
+++ TestClass3.java
@@ -0,0 +1,16 @@
+package demoGitPackage;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "Yes" || hungry == "z")
+			System.out.println("eat Something");
+		else {
+			System.out.println("Do Work...");
+System.out.println("Do Work...fdfdfdfd");
+		}
+
+	}
+
+}
